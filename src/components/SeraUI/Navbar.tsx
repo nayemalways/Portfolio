ffeb2646be8b6898112677c1cd6b856379246743
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-08-28 21:49:27 +0600

    fixed typescript error

--- src/components/SeraUI/Navbar.tsx
+++ src/components/SeraUI/Navbar.tsx
@@ -1,20 +1,10 @@
 import DockApp from '../ui/dock';
-import { DockIcon, EditIcon, GithubIcon, HomeIcon } from 'lucide-react';
- 
+
+
  const Navbar = () => {
   return (
     <div>
-       <DockApp>
-          <DockIcon href="/home">
-            <HomeIcon className="h-full w-full p-2 text-zinc-700 dark:text-zinc-300" />
-          </DockIcon>
-          <DockIcon href="/edit">
-            <EditIcon className="h-full w-full p-2 text-zinc-700 dark:text-zinc-300" />
-          </DockIcon>
-          <DockIcon href="https://github.com">
-            <GithubIcon className="h-full w-full p-2 text-zinc-700 dark:text-zinc-300" />
-          </DockIcon>
-      </DockApp>
+       <DockApp />
     </div>
   );
  };
